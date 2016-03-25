5fb1bf59a303654cc68b9507e2486f6df0319311
BhanuKaplish <bhanu.kaplish@gmail.com>
2016-03-25 16:45:43 -0400

    Added Scoreboard

---
+++ 3DGameDemo/Assets/_Scripts/GameController.cs
@@ -0,0 +1,113 @@
+﻿///<File Name: GameController.cs>
+///<Author Name: Bhanu Kaplish>
+///<Description: This script is attached to the game controller object which controls features like labels>
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    //Private Instance variables
+    private int _scoreValue;
+    private int _livesValue;
+    //[SerializeField]
+    //private AudioSource _gameoverSound;
+
+    //private Variables
+    private Vector3 _playerSpawnPoint;
+
+    // PUBLIC ACCESS METHODS
+    public int ScoreValue
+    {
+        get
+        {
+            return this._scoreValue;
+        }
+
+        set
+        {
+            this._scoreValue = value;
+            this.ScoreLabel.text = "Score: " + this._scoreValue;
+        }
+    }
+
+    public int LivesValue
+    {
+        get
+        {
+            return this._livesValue;
+        }
+
+        set
+        {
+            this._livesValue = value;
+            if (this._livesValue <= 0)
+            {
+                this._endGame();
+            }
+            else {
+                this.LivesLabel.text = "lives: " + this._livesValue;
+            }
+        }
+    }
+
+
+
+    //Public Instance Variables
+    //public int asteroidNumber = 4;
+    //public PlaneController plane;
+    //public CoinController coin;
+    //public AsteroidController asteroid;
+    public Text LivesLabel;
+    public Text ScoreLabel;
+    public Text GameOverLabel;
+    public Text HighscoreLabel;
+    public Button RestartButton;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start() {
+        initialize();
+
+        Instantiate(this.player, this._playerSpawnPoint, Quaternion.identity);
+    }
+
+// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Private method
+    private void initialize()
+    {
+        this._playerSpawnPoint = new Vector3(8.0f, 2.0f, 20.0f);
+        this._livesValue = 5;
+        this._scoreValue = 0;
+        this.GameOverLabel.enabled = false;
+        this.HighscoreLabel.enabled = false;
+        this.RestartButton.gameObject.SetActive(false);
+
+        //for (int asteroidCount = 0; asteroidCount < this.asteroidNumber; asteroidCount++)
+        //{
+        //    Instantiate(asteroid.gameObject);
+        //}
+        
+    }
+    private void _endGame()
+    {
+        this.HighscoreLabel.text = "Highscore : " + this._scoreValue;     
+        this.GameOverLabel.enabled = true;
+        this.HighscoreLabel.enabled = true;
+        this.LivesLabel.enabled = false;
+        this.ScoreLabel.enabled = false;
+        //this.plane.gameObject.SetActive(false);
+        //this.coin.gameObject.SetActive(false);
+        this.RestartButton.gameObject.SetActive(true);
+        //this._gameoverSound.Play();
+    }
+    //Public methods
+    //public void RestartButtonClick()
+    //{
+    //    Application.LoadLevel("Main");
+    //}
+}
---
+++ 3DGameDemo/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿///<File Name: PlayerController.cs>
+///<Author Name: Bhanu Kaplish>
+///<Description: This script is attached to the player of the game which handles the labels using gameController's object>
+
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Public Instance Variables
+    public GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //if(other.gameObject.CompareTag("Enemy"))
+        //{
+        //    this._asteroidSound.Play();
+        //    this.gameController.livesValue -= 1;
+        //}
+        if (other.gameObject.CompareTag("Heart"))
+        {
+            //this._asteroidSound.Play();
+            this.gameController.LivesValue += 1;
+        }
+        if (other.gameObject.CompareTag("Coins"))
+        {
+            //this._coinSound.Play();
+            this.gameController.ScoreValue += 100;
+            Destroy(gameObject);
+        }
+    }
+}
