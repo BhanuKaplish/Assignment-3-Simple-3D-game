bd989f2b6378ff66dc3b31b263608d1b3529b827
BhanuKaplish <bhanu.kaplish@gmail.com>
2016-03-25 18:41:58 -0400

    Lives Score and Hurt Sound are working

---
+++ 3DGameDemo/Assets/_Scripts/DeathPlaneController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathPlaneController : MonoBehaviour {
+    //Public Instance Variables
+    public Transform spawnPoint;
+    public GameController gameController;
+
+    //Private Instance Variables
+    private AudioSource _HurtSound;
+
+
+	 
+    // Use this for initialization
+	void Start () {
+        this._HurtSound = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(this.spawnPoint.position);
+            Transform playerTransform = other.gameObject.GetComponent<Transform>();
+            playerTransform.position = this.spawnPoint.position;
+            gameController.LivesValue -= 1;
+            this._HurtSound.Play();
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
